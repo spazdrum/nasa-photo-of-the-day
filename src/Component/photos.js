49ca31157b41ce164e795712c86ee1e3eed5d348
spazdrum <spazdrum@gmail.com>
2020-04-14 23:40:09 -0400

    completed step 1

---
+++ src/Component/photos.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function Photos () {
+    const [photo, setPhoto] = useState([])
+    useEffect (() => {
+        axios
+        .get("https://api.nasa.gov/planetary/apod?api_key=YVPagJ9qLby5sdcU77njZ5v0s9PVybf68UkwJlKE=2020-10-03")
+        .then(response => {
+            console.log(response)
+            setPhoto(response.data)
+        }) 
+    }, [])
+
+    return (
+        <div>
+            <title>{photo.title}</title>
+            <h3>{photo.date}</h3>
+            <img src={photo.hdurl} alt="" />
+            <h4>{photo.explanation}</h4>
+        </div>
+    )
+}
+
+export default Photos;
